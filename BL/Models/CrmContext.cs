a55f21fbf73b6bc678846f875797df4dba5e380c
LTs <danil.maskin@inbox.ru>
2021-01-25 17:10:02 +0300

    Added business logic and database.

---
+++ BL/Models/CrmContext.cs
@@ -0,0 +1,15 @@
+﻿using CrmBL.Models;
+using System.Data.Entity;
+
+namespace BL.Models
+{
+    class CrmContext : DbContext
+    {
+        public CrmContext() : base("LTsCrmDB") { }
+        public DbSet<Check> Checks { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sell> Sells { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+    }
+}
---
+++ BL/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CrmBL.Models
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public uint Count { get; set; }
+        public virtual ICollection<Sell> Sells { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
